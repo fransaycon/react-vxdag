e5bc51c205099460aa2c3597356c15338416f3e6
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2020-10-04 18:20:56 +0800

    Create First Prototype Node

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
+import DefaultNode from './Node/DefaultNode';
 
-const Container = styled.div`
-  background-color: blue;
-  color: white;
-  width: 500px;
-  height: 200px;
+const Container = styled.svg`
+  width: 100vw;
+  height: 100vh;
+  background-color: #e2e2e2;
 `;
 
 const Sample: React.FC = () => (
   <Container>
-    <h1>Hello World</h1>
+    <DefaultNode text={'Hello World'} height={200} width={200} padding={20} />
   </Container>
 );
 
